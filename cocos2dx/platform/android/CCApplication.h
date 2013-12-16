af85ab618f785e398ee212eda78b0cd6d36be09d
Fedor Shubin <shubin.fedor@gmail.com>
2013-12-16 18:23:32 +0400

    fix Android build

--- cocos2dx/platform/android/CCApplication.h
+++ cocos2dx/platform/android/CCApplication.h
@@ -36,7 +36,13 @@ public:
     @return Current language config
     */
     virtual ccLanguageType getCurrentLanguage();
-    
+
+    /**
+     @brief Get current language iso 639-1 code
+     @return Current language iso 639-1 code
+     */
+    virtual const char * getCurrentLanguageCode();    
+
     /**
      @brief Get target platform
      */
