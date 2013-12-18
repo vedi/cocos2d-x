e623adfcd4318735986fa7c830ca04347f0d377d
Fedor Shubin <shubin.fedor@gmail.com>
2013-12-18 14:31:12 +0400

    fix velocity of auto animations in CB

--- extensions/CCBReader/CCNodeLoader.cpp
+++ extensions/CCBReader/CCNodeLoader.cpp
@@ -932,7 +932,7 @@ CCNode * CCNodeLoader::parsePropTypeCCBFile(CCNode * pNode, CCNode * pParent, CC
     if (ccbFileNode && ccbReader->getAnimationManager()->getAutoPlaySequenceId() != -1)
     {
         // Auto play animations
-        ccbReader->getAnimationManager()->runAnimationsForSequenceIdTweenDuration(ccbReader->getAnimationManager()->getAutoPlaySequenceId(), 0, true);
+        ccbReader->getAnimationManager()->runAnimationsForSequenceIdTweenDuration(ccbReader->getAnimationManager()->getAutoPlaySequenceId(), 0, 0);
     }
     
     if (ccbReader->isJSControlled() && pCCBReader->isJSControlled() && NULL == ccbReader->mOwner)
